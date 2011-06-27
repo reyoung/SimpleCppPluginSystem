b9c82e0a3b30a8455f984f7eebfc5216a30a6979
reyoung <reyoung@126.com>
2011-06-27 15:54:51 +0800

    LoaderPool Added

--- ExtentionSystem/PluginLoader_Win32.cpp
+++ ExtentionSystem/PluginLoader_Win32.cpp
@@ -1,8 +1,45 @@
 #include "PluginLoader_Win32.h"
 
+#include <map>
+
+using namespace std;
+
+struct AUX_STRUCT{
+	int	refcount;
+	PluginLoaderPrivate*	 data;
+	AUX_STRUCT()
+		:refcount(0),data(0)
+	{
+	}
+};
+static map<string,AUX_STRUCT> LoaderPool;
+
+static void CleanLoaderPool(){
+	for (map<string,AUX_STRUCT>::iterator it = LoaderPool.begin();it!=LoaderPool.end();++it)
+	{
+		delete it->second.data;
+	}
+}
+static int RegestExitFunc(){
+	atexit(CleanLoaderPool);
+	return 0;
+}
+static int __InitValue = RegestExitFunc();
+
 PluginLoaderPrivate* PluginLoaderPrivate::findOrCreate( const string& fn )
 {
-    return 0;
+	if(LoaderPool.find(fn)==LoaderPool.end()){  //! 未加载过
+		AUX_STRUCT temp;
+		temp.data = new PluginLoaderPrivate();
+		temp.data->filename = fn;
+		++temp.refcount;
+		LoaderPool[fn]=temp;
+		return temp.data;
+	}else{
+		AUX_STRUCT& temp = LoaderPool[fn];
+		++temp.refcount;
+		return temp.data;
+	}
 }
 
 PluginLoaderPrivate::PluginLoaderPrivate()
@@ -12,7 +49,10 @@ PluginLoaderPrivate::PluginLoaderPrivate()
 
 PluginLoaderPrivate::~PluginLoaderPrivate()
 {
-
+	if (pHnd)
+	{
+		FreeLibrary(this->pHnd);
+	}
 }
 
 bool PluginLoaderPrivate::load()
@@ -32,5 +72,15 @@ Castable* PluginLoaderPrivate::getInstance() const
 
 bool PluginLoaderPrivate::unuse()
 {
-    return false;
+	if(LoaderPool.find(this->filename)==LoaderPool.end()){ // 没找到
+		return false;
+	}else{
+		AUX_STRUCT& temp = LoaderPool[this->filename];
+		--temp.refcount;
+		if(!temp.refcount){	//! 没有人在引用了
+			LoaderPool.erase(LoaderPool.find(this->filename));  // 将容器中的引用删除
+			delete this; // 删除自己
+		}
+		return true;
+	}
 }
--- Main/main.cpp
+++ Main/main.cpp
@@ -1,6 +1,9 @@
-#include <stdio.h>
-#include <ExtentionSystem/Castable.h>
+#include <iostream>
+#include <ExtentionSystem/PluginLoader.h>
+using namespace std;
 int main(){
-
+	PluginLoader ploader;
+	ploader.setFileName("PluginExample");
+	cout<<ploader.unload()<<endl;
 	return 0;
 }
