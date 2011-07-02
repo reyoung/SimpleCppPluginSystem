357ffbb7a9d3efc98b09b42a47d687451a2d3bb6
reyoung <reyoung@126.com>
2011-07-02 22:20:33 +0800

    add Helper Function

--- CommandPlugin/ICommandSet.cpp
+++ CommandPlugin/ICommandSet.cpp
@@ -1,9 +1,19 @@
 #include "ICommandSet.h"
-
+#include "CommandPool.h"
+#include "KeyCommandSet.h"
+using namespace std;
 ICommandSet::ICommandSet(void)
 {
 }
 
 ICommandSet::~ICommandSet(void)
 {
+}
+
+void ICommandSet::newCommandWithType( const std::string& type,const std::string& key,const map<std::string,Variant>& param )
+{
+	if (type=="Key")
+	{
+		CommandPool::Instance()->getKeyCommandSet().newCommand(key,param);
+	}
 }
--- CommandPlugin/ICommandSet.h
+++ CommandPlugin/ICommandSet.h
@@ -6,4 +6,5 @@ class COMMANDPLUGIN_API ICommandSet:public Castable
 public:
 	ICommandSet(void);
 	virtual ~ICommandSet(void);
+	void newCommandWithType(const std::string& type,const std::string& key,const std::map<std::string,Variant>& param);
 };
